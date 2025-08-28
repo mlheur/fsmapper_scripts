be509c71cba2502d32ac22626fff2eb28f541bd7
mlheur <mlheur@gmail.com>
2025-08-27 22:22:38 -0700

    feat: generic device manager abstraction layer

---
+++ lib/devices.lua
@@ -0,0 +1,23 @@
+local mgr = {}
+
+function mgr.add_mappings(model,mapper_device)
+    for input_name,event_map in pairs(mapper_device.map) do
+        for action_name,model_map in pairs(event_map) do
+            mapper.print(
+                "Adding device event map,"..
+                " device:["..mapper_device.name.."]"..
+                " model:["..model.."]"..
+                " input:["..input_name.."]"..
+                " action:["..action_name.."]"..
+                " RPN:["..model_map[model].."]"..
+                " event:["..mapper_device.map[input_name][action_name].__event__.."]"
+            )
+            mapper.add_secondary_mappings({{
+                event = mapper_device.map[input_name][action_name].__event__,
+                action = msfs.mfwasm.rpn_executer(model_map[model]),
+            }})
+        end
+    end
+end
+
+return mgr
