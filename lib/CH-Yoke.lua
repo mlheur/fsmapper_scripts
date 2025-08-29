7513e14d0cdf243cb0eda3dd1c5e6507fa572fa2
mlheur <mlheur@gmail.com>
2025-08-28 22:59:57 -0700

    refactoring: while mapping more axes to darkstar cockpit touchables

--- lib/CH-Yoke.lua
+++ lib/CH-Yoke.lua
@@ -7,8 +7,19 @@ Yoke.device = mapper.device{
     type = 'dinput',
     identifier = {guid = '{1DDD4F20-8387-11ED-8001-444553540000}'},
     modifiers = {
+        { name = "button7", modtype = 'button' },
+        { name = "button8", modtype = 'button' },
         {
             name = 'ry',
+            modtype = 'quantized_stick',
+            modparam = {
+                repeat_mode = false,
+                activate_threshold = 48000,
+                release_threshold  = 48001,
+            }
+        },
+        {
+            name = 'rx',
             modtype = 'quantized_stick',
             modparam = {
                 repeat_mode = false,
@@ -20,13 +31,34 @@ Yoke.device = mapper.device{
 }
 
 Yoke.map = {}
+Yoke.map.rx = {}
+Yoke.map.rx.positive = {}
+Yoke.map.rx.negative = {}
+
 Yoke.map.ry = {}
 Yoke.map.ry.positive = {}
-Yoke.map.ry.positive.__event__ = Yoke.device.events.ry.positive
 Yoke.map.ry.negative = {}
-Yoke.map.ry.negative.__event__ = Yoke.device.events.ry.negative
+
+Yoke.map.button7 = {}
+Yoke.map.button7.down = {}
+
+Yoke.map.button8 = {}
+Yoke.map.button8.down = {}
 
 Yoke.map.ry.positive.Darkstar = msfs.input_event_executer('HANDLING_Spoilers', 0)
 Yoke.map.ry.negative.Darkstar = msfs.input_event_executer('HANDLING_Spoilers', 1)
+
+Yoke.map.rx.positive.Darkstar = function(evid,args)
+    msfs.execute_input_event('ELECTRICAL_Fuel_Cell', 1)
+    msfs.execute_input_event('COMMON_Cover_Transition', 0)
+end
+
+Yoke.map.rx.negative.Darkstar = function(evid,args)
+    msfs.execute_input_event('ELECTRICAL_Fuel_Cell', 1)
+    msfs.execute_input_event('COMMON_Cover_Transition', 1)
+end
+
+Yoke.map.button7.down.Darkstar = msfs.input_event_executer('ENGINE_Transition', 1)
+Yoke.map.button8.down.Darkstar = msfs.input_event_executer('ENGINE_Transition', 0)
 
 return Yoke
--- lib/devices.lua
+++ lib/devices.lua
@@ -3,17 +3,17 @@ local mgr = {}
 function mgr.add_mappings(model,mapper_device)
     for input_name,event_map in pairs(mapper_device.map) do
         for action_name,model_map in pairs(event_map) do
+            if model_map[model] == nil then return end
             mapper.print(
                 "Adding device event map,"..
                 " device:["..mapper_device.name.."]"..
                 " model:["..model.."]"..
                 " input:["..input_name.."]"..
-                " action:["..action_name.."]"..
-                " event:["..mapper_device.map[input_name][action_name].__event__.."]"
+                " action:["..action_name.."]"
             )
             mapper.add_secondary_mappings({{
-                event = mapper_device.map[input_name][action_name].__event__,
-                action = model_map[model],
+                event  = mapper_device.device.events[input_name][action_name],
+                action = model_map[model]
             }})
         end
     end
