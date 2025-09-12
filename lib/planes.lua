d39537402877613b18ea8336f2956c32701ef43f
mlheur <mlheur@gmail.com>
2025-09-12 16:30:14 -0700

    fix: typo in Xcub

--- lib/planes.lua
+++ lib/planes.lua
@@ -84,7 +84,7 @@ local function get_make_model(aircraft)
     if first_two_words == "Asobo LS8"             then return "MXS",model end
     if first_two_words == "Asobo NXCub"          then return "CC19",model end
     if first_two_words == "Asobo XCub"            then return "CC19",model end
-    if first_word      == "XCub"                  then return "CC19",model end
+    if first_word      == "Xcub"                  then return "CC19",model end
     if first_word      == "Pitts"                 then return "PTS2",model end
     if first_two_words == "Orbis Asobo"           then return "Orbis",model end
     if first_two_words == "Asobo Savage"          then return "SAVG",model end
