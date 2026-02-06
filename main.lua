2015a5c51356ff7b0eba699310a47fb92283166d
mlheur <mlheur@gmail.com>
2026-02-06 15:37:39 -0800

    refactor: changing code layout and naming styles

--- main.lua
+++ main.lua
@@ -1,17 +1,17 @@
 device_mgr = require("lib/devices")
 action_mgr = require("lib/actions")
-plane_mgr = require("lib/planes")
+plane_mgr  = require("lib/planes")
 
 --------------------------------------------------------------------------------
 -- Main: Reload everything whenever the aircraft is changed.
 --------------------------------------------------------------------------------
-mapper.set_primary_mappings{
-    {
-        event=mapper.events.change_aircraft,
-        action = function (evid,args)
-            if args.aircraft == nil then return end
-            mapper.set_secondary_mappings{}
-            plane_mgr.add_mappings(args.aircraft)
-        end
-    },
-}
+local function onChangeAircraft(evid,args)
+    if args.aircraft == nil then return end
+    mapper.set_secondary_mappings( {} )
+    plane_mgr.add_mappings(args.aircraft)
+end
+
+tTopLevelMap = {}
+table.insert( tTopLevelMap, { event = mapper.events.change_aircraft, action = onChangeAircraft } )
+
+mapper.set_primary_mappings(tTopLevelMap)
